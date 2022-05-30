af21ce9e2835e93216da94f23f3d26d0f8da58c3
Arthur Marques <arthur.l2@hotmail.com>
2022-05-29 23:08:10 -0300

    Criando guard da funcao build_from_many

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -20,6 +20,10 @@ defmodule ReportsGenerator do
     |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
   end
 
+  def build_from_many(filenames) when not is_list(filenames) do
+    {:error, "Please provide a list of strings"}
+  end
+
   def build_from_many(filenames) do
     filenames
     |> Task.async_stream(&build/1)
